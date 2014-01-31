03ba28297f4b5781aa3437768dedc416b32095e1
Kromey <travisvz@gmail.com>
2014-01-30 16:19:24 -0900

    Framework in place for Choose Ogre buttons to generate a new Ogre

--- ogreCompany.js
+++ ogreCompany.js
@@ -10,11 +10,22 @@ var ogreCompany = {
 			newOgreType.id = 'new_'+type;
 			newOgreType.value = type;
 			newOgreType.appendChild(textNode(ogreTypes[type].name));
+			newOgreType.onclick = function(){ogreCompany.addOgre(this.value);};
 
 			newOgre.appendChild(newOgreType);
 		}
 
 		document.body.appendChild(newOgre);
+	},
+
+	addOgre : function(type) {
+		if(ogreTypes[type] == undefined)
+		{
+			console.log("Unknown Ogre type: "+type);
+			return false;
+		}
+
+		alert('Making new Ogre '+ogreTypes[type].name);
 	}
 }
 
