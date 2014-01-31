34251c74fba3f62d9453fa7fe08ad8c21a526c7e
Kromey <travisvz@gmail.com>
2014-01-30 16:01:57 -0900

    Initial work in dynamically generating "New Ogre" buttons

--- ogreCompany.js
+++ ogreCompany.js
@@ -1,4 +1,48 @@
 var ogreCompany = {
+	init : function() {
+		var newOgre = document.createElement('div');
+		newOgre.id = 'newOgreDiv';
+		newOgre.appendChild(document.createTextNode("Choose an Ogre:"));
+
+		for(var type in ogreTypes)
+		{
+			var newOgreType = document.createElement('button');
+			newOgreType.id = 'new_'+type;
+			newOgreType.value = type;
+			newOgreType.appendChild(document.createTextNode(ogreTypes[type].name));
+
+			newOgre.appendChild(newOgreType);
+		}
+
+		document.body.appendChild(newOgre);
+	}
+}
+
+var ogreArmaments = {
+	"mb":{
+		"name":"Main Battery",
+		"attack":4,
+		"range":3,
+		"defense":4
+	},
+	"sb":{
+		"name":"Secondary Battery",
+		"attack":3,
+		"range":2,
+		"defense":3
+	},
+	"msl":{
+		"name":"Missiles",
+		"attack":6,
+		"range":5,
+		"defense":3
+	},
+	"ap":{
+		"name":"Antipersonnel",
+		"attack":1,
+		"range":1,
+		"defense":1
+	}
 }
 
 var ogreTypes =
@@ -56,33 +100,6 @@ var ogreTypes =
 				"count":12
 			}
 		}
-	}
-}
-
-ogreArmaments = {
-	"mb":{
-		"name":"Main Battery",
-		"attack":4,
-		"range":3,
-		"defense":4
-	},
-	"sb":{
-		"name":"Secondary Battery",
-		"attack":3,
-		"range":2,
-		"defense":3
-	},
-	"msl":{
-		"name":"Missiles",
-		"attack":6,
-		"range":5,
-		"defense":3
-	},
-	"ap":{
-		"name":"Antipersonnel",
-		"attack":1,
-		"range":1,
-		"defense":1
 	}
 }
 
