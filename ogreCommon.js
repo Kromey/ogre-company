b0fa4021a1ed9cd7afba2ee85dc10d0c665ad975
Travis Veazey <travisvz@gmail.com>
2014-02-05 19:38:15 -0900

    Added new destroyNode utility/helper function

--- ogreCommon.js
+++ ogreCommon.js
@@ -12,3 +12,18 @@ function getId(id)
 {
 	return document.getElementById(id);
 }
+
+function destroyNode(nodeRef)
+{
+	if(typeof nodeRef == 'object')
+	{
+		var node = nodeRef;
+	} else {
+		var node = getId(nodeRef);
+	}
+	
+	if(node && node.parentNode)
+	{
+		node.parentNode.removeChild(node);
+	}
+}
